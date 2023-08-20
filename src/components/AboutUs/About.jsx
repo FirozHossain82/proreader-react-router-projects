a650fb5108edba6a2925fac6e361bf9c8f977723
FirozHossain82 <firozhossain295@gmail.com>
2023-08-20 13:36:44 +0600

    home section added

--- src/components/AboutUs/About.jsx
+++ src/components/AboutUs/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
            <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
       <div className='max-w-xl sm:mx-auto lg:max-w-2xl'>
         <div className='max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12'>
-          <h2 className='max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto'>
-            <span className='relative inline-block'>
+          <h2 className='max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-green-700 sm:text-4xl md:mx-auto'>
+            {/* <span className='relative inline-block'>
               <svg
                 viewBox='0 0 52 24'
                 fill='currentColor'
@@ -30,8 +30,8 @@ const About = () => {
                 />
               </svg>
               <span className='relative'>A</span>
-            </span>
-            reader lives a thousand lives before he dies
+            </span> */}
+            A reader lives a thousand lives before he dies
           </h2>
           <p className='text-base text-gray-700 md:text-lg'>
             Books are a uniquely portable magic. Books serve to show a man that
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
           <ul className="flex items-center hidden space-x-8 lg:flex ">
             <li>
               <Link
-                to="/home"
+                to="/"
                 aria-label="Home"
                 title="Home"
                 className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400 hover:text-orange-500 "
