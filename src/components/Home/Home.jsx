df890cdee0db802632f580cc5a87bc08eebc211f
FirozHossain82 <firozhossain295@gmail.com>
2023-08-21 12:10:39 +0600

    footer added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import reader from '../../assets/reader.json'
 const Home = () => {
   return (
-    <div className='bg-cyan-50'>
+    <div className='bg-cyan-50 pb-20'>
         <div className=' px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-16'>
       <div className='flex flex-col items-center justify-between lg:flex-row' >
         <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0 ">
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header.jsx';
 import { Outlet } from 'react-router-dom';
+import { Footer } from '../Footer/Footer.jsx';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
